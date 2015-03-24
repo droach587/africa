25a6a3643afb95a077bbe4e1929654f08540f207
David Roach <Droach587@gmail.com>
2015-03-23 23:11:10 -0400

    Adding
    Signed-off-by: David Roach <Droach587@gmail.com>

--- config.rb
+++ config.rb
@@ -3,12 +3,12 @@ require "compass-growl"
 
 # Set this to the root of your project when deployed:
 http_path = ""
-css_dir = "/css"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "/img"
-generated_images_dir = "/img"
-javascripts_dir = "/js"
-fonts_dir = "/css/fonts"
+images_dir = "img"
+generated_images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "css/fonts"
 
 output_style = :compact
 
